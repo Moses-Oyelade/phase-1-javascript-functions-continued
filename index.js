56d403d822ceee2f32cf0671276b4fe517c1b983
Moses <moses.oyelade@student.moringaschool.com>
2023-08-03 22:26:16 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(target= "roller-skate"){
+    return `This Saturday, I want to ${target}!`
+}
+saturdayFun();
+
+saturdayFun("bathe my dog");
+
+function mondayWork(target = 'go to the office'){
+return `This Monday, I will ${target}.`
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(result = '*'){
+    return function(adjective = "a hard worker"){
+        return `You are ${result}${adjective}${result}!`
+    }
+
+}
+wrapAdjective(("||")("a dedicated programmer"));
